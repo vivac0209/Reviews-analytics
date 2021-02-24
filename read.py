ef4d6ff44fe9d1db2e64c4a23ddf828bca2e69fa
Viva Chan <vivachan29@gmail.com>
2021-02-24 14:03:20 +0800

    updatefilter function

--- read.py
+++ read.py
@@ -1,13 +1,26 @@
 data=[]
-count=0
+x=0
+z=0
 with open('reviews.txt' , 'r') as f:
 	for line in f:
 		data.append(line)
-		count+=1
-		if count % 1000 ==0:
-			print(len(data))
+		
+		
 
-print(len(data))
-print(data[0])
-print('---------------')
-print(data[1])
+print('檔案讀取完了,總共有',len(data),'筆資料')
+
+for i in data:
+	z=len(i)
+	x=x+z
+
+answer= x/1000000
+print('平均留言長度為:' , answer)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print('一共有', len(new),'筆留言長度小於100')
+
+
